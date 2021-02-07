92f5b1bd461aa108df87b38731ac9ed4300ff0d4
Jonas Læborg <jonas@laeborg.com>
2021-02-07 23:53:23 +0100

    Initial commit

---
+++ custom_components/linak_dpg/config_flow.py
@@ -0,0 +1,8 @@
+"""
+Config Flow for the Linak DPG integration
+"""
+
+from homeassistant.core import callback
+from homeassistant.components import mqtt
+
+import logging
---
+++ custom_components/linak_dpg/const.py
@@ -0,0 +1,5 @@
+"""
+Constants for the Linak DPG integration
+"""
+
+DOMAIN = "linak_dpg"
