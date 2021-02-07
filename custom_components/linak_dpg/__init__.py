1d93704e5df8a1d39462087c83de220ecf93851b
Jonas Læborg <jonas@laeborg.com>
2021-02-08 00:18:45 +0100

    Add test sensor

--- custom_components/linak_dpg/__init__.py
+++ custom_components/linak_dpg/__init__.py
@@ -4,3 +4,19 @@ Linak DPG Desk Panel Integration
 For more details about this integration, please refer to the documentation at
 https://github.com/Laeborg/hass-linak-dpg
 """
+
+from homeassistant import core
+from homeassistant.components import mqtt
+
+import logging
+
+def setup(hass, config):
+    """Your controller/hub specific code."""
+    
+    hass.data[DOMAIN] = {
+        'height': 0
+    }
+
+    hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
+
+    return True
--- custom_components/linak_dpg/config_flow.py
+++ custom_components/linak_dpg/config_flow.py
@@ -3,6 +3,12 @@ Config Flow for the Linak DPG integration
 """
 
 from homeassistant.core import callback
-from homeassistant.components import mqtt
 
 import logging
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Optional(CONF_NAME): cv.name,
+        vol.Optional(CONF_MAC_ADDRESS): cv.mac_address
+    }),
+}, extra=vol.ALLOW_EXTRA)
