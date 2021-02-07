cc18ae4962e63f8bcc517a89fa4a35d3960d3b93
Jonas Læborg <jonas@laeborg.com>
2021-02-08 00:21:45 +0100

    Remove config flow

--- custom_components/linak_dpg/__init__.py
+++ custom_components/linak_dpg/__init__.py
@@ -10,6 +10,13 @@ from homeassistant.components import mqtt
 
 import logging
 
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Optional(CONF_NAME): cv.name,
+        vol.Optional(CONF_MAC_ADDRESS): cv.mac_address
+    }),
+}, extra=vol.ALLOW_EXTRA)
+
 def setup(hass, config):
     """Your controller/hub specific code."""
     
