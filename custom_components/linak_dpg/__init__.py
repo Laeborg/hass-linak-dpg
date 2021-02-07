4e4c95f8d55ee9fa715dcf1c3ddeb3e0f80fbda9
Jonas Læborg <jonas@laeborg.com>
2021-02-07 23:43:52 +0100

    Cookiecutter

--- custom_components/linak_dpg/__init__.py
+++ custom_components/linak_dpg/__init__.py
@@ -5,4 +5,10 @@ For more details about this integration, please refer to the documentation at
 https://github.com/Laeborg/hass-linak-dpg
 """
 
+from homeassistant.core import callback
+from homeassistant.components import mqtt
+
 import logging
+
+# Domain configuration
+DOMAIN = "linak_dpg"
