dbf1997e31e96b190b2cc6790a18cbdf0957487a
Jonas Læborg <jonas@laeborg.com>
2021-02-07 23:31:15 +0100

    Add init file

---
+++ custom_components/linak_dpg/__init__.py
@@ -0,0 +1,8 @@
+"""
+Linak DPG Desk Panel Integration
+
+For more details about this integration, please refer to the documentation at
+https://github.com/Laeborg/hass-linak-dpg
+"""
+
+import logging
