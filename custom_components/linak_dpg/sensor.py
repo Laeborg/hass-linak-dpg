bcb1b6c69e6fdba4d63c195fdd87c604fb0d1a17
Jonas Læborg <jonas@laeborg.com>
2021-02-11 21:00:16 +0100

    Continuously update the connection status

--- custom_components/linak_dpg/sensor.py
+++ custom_components/linak_dpg/sensor.py
@@ -5,6 +5,11 @@ Platform for Linak DPG Desk Panel Integration
 import random
 
 from homeassistant.helpers.entity import Entity
+from homeassistant.const import (
+    STATE_OFF,
+    STATE_ON,
+)
+
 from .const import DOMAIN, LOGGER
 from .bluetoothctl import Bluetoothctl
 
@@ -22,10 +27,24 @@ class DeskSensor(Entity):
         self._name = config_entry.data.get("name")
         self._uuid = config_entry.data.get("id")
         self._address = config_entry.data.get("address")
-        self._height = random.randint(60, 130)
+        self._height = self.height
         self._unit_of_measurement = "cm"
-        self._availability = "Off"
-        self._state = state()
+        self._state = None
+        
+    def update(self):
+        """Update state of the device."""
+        try:
+            connection = Bluetoothctl().connect(self._address)
+            
+            if not connection:
+                raise Exception(f"Failed to establish connection")
+                
+        except Exception as e:
+            LOGGER.error(e)
+            self._state = STATE_OFF
+            
+        else:
+            self._state = STATE_ON
         
     @property
     def unique_id(self) -> str:
@@ -38,24 +57,19 @@ class DeskSensor(Entity):
         return self._name
 
     @property
+    def state(self):
+        """Return the state of the device."""
+        return self._state
+
+    @property
     def address(self):
         """Return the address of the device."""
         return self._address
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        connection = Bluetoothctl().connect(self._address)
         
-        if connection:
-            return True
-            
-        return False
-
     @property
-    def availability(self):
-        """Return the availability of the device."""
-        return self._availability
+    def height(self):
+        """Return the current height of the device."""
+        return random.randint(60, 130)
         
     @property
     def device_state_attributes(self):
@@ -64,6 +78,5 @@ class DeskSensor(Entity):
         attr["height"] = self._height
         attr["unit_of_measurement"] = self._unit_of_measurement
         attr["mac_address"] = self._address
-        attr["availability"] = self._availability
         
         return attr
